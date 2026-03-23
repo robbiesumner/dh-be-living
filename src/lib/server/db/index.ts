ceded23e246b78daa88334f9e0dec7d501e180ee
Robbie Sumner <robbiecsumner@gmail.com>
2026-03-23 02:03:36 +0100

    fix: don't check db env on building

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,9 +2,10 @@ import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
+import { building } from '$app/environment';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!env.DATABASE_URL && !building) throw new Error('DATABASE_URL is not set');
 
-const client = createClient({ url: env.DATABASE_URL });
+const client = createClient({ url: env.DATABASE_URL || 'file:sqlite.db' });
 
 export const db = drizzle(client, { schema });
